d4ede1e16cc9e4f398f8865248d154425282d6f8
Nishant Singh <nishant.singh2@socgen.com>
2015-01-05 13:03:36 +0000

    adding db connection

--- app.js
+++ app.js
@@ -16,11 +16,36 @@ app.listen(port);
 var router = express.Router();
 var rootURL = process.env.rootURL || '/api';
 app.use(rootURL, router);
+
 console.log("Listening on url : " + rootURL + "/" + port)
 
 // Add a requrest handler
 router.get('/', function(req, res) {
-    res.json({ message: 'geeksaint.com API!' });   
+  res.json({ message: 'geeksaint.com API!' });   
 });
 
 /*  ***************** DB config ***************  */
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/gsb_dev'); 
+
+//Test db connection
+var Bear     = require('./app/models/bear');
+
+router.route("/bears")
+  .get(function(req, res) {
+    res.json({
+      message: 'here is your bear'
+    });
+  })
+  .post(function(req, res){
+  var bear = new Bear();
+  bear.name = req.body.name;
+  
+  bear.save(function(err) {
+    if(err){
+        res.send(err);
+    }
+    res.json({ message: 'Bear created! with name: ' + bear.name });
+  });
+  
+});
